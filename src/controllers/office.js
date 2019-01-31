8f92e5441f183180285f241d379429748d18d847
sengayire <psengayire@gmail.com>
2019-01-31 09:56:20 +0200

    #163589166-get-all-political-offices

--- src/controllers/office.js
+++ src/controllers/office.js
@@ -1,16 +1,18 @@
+import Office from '../data/office';
+
 const office = {
 
   async create(req, res) {
     const { id, type, name } = req.body;
 
     if (!type || !name) {
-      res.send({
+      res.status(200).send({
         status: 400,
         message: 'please provide all information',
       });
     } else {
       try {
-        res.send({
+        res.status(200).send({
           status: 200,
           message: 'office created successfuly',
           data: {
@@ -25,6 +27,19 @@ const office = {
           error,
         });
       }
+    }
+  },
+  async getAll(req, res) {
+    try {
+      res.status(200).send({
+        status: 200,
+        data: Office,
+      });
+    } catch (error) {
+      res.status(404).send({
+        status: 404,
+        error,
+      });
     }
   },
 };
---
+++ src/data/office.js
@@ -0,0 +1,12 @@
+const office = [{
+  id: 1,
+  type: 'hq',
+  name: 'fpr',
+},
+{
+  id: 2,
+  type: 'hq',
+  name: 'pr',
+}];
+
+export default office;
