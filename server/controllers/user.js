b5deaf8a554b0c747cbb3ae3399d9a3b178b451b
sengayire <psengayire@gmail.com>
2019-02-19 21:56:43 +0200

    [ft-164061807]-add-jwt-authantication

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,17 +1,18 @@
 /* eslint-disable no-multi-assign */
 import uuid from 'uuid';
 import sqlQueries from '../models/database/queries';
-// import connect from '../models/database/database';
-import connect from '../models/database/index';
+import database from '../models/database/database';
+import connect from '../models/database';
+import helper from '../helpers/helper';
 
 const user = {
   // create user table
   async createTable() {
     const table = sqlQueries.userTable;
-    const execute = connect.query(table)
+    const execute = database.query(table)
       .then((res) => {
         console.log(res);
-        connect.end();
+        database.end();
       })
       .catch((err) => {
         console.log(err);
@@ -27,10 +28,11 @@ const user = {
       lastName,
       otherName,
       email,
-      password,
       phoneNumber,
       passportUrl,
     } = req.body;
+
+    const hasedPassword = helper.hashPassword(req.body.password);
 
     const data = [
       uuid(),
@@ -38,22 +40,34 @@ const user = {
       lastName,
       otherName,
       email,
-      password,
+      hasedPassword,
       phoneNumber,
       passportUrl,
     ];
+
+    if (!helper.isValidEmail(email)) {
+      return res.status(400).send({ message: 'Please enter a valid email address' });
+    }
+
     const search = sqlQueries.selectAll;
-    let get = [];
-    get = await connect.query(search, [email]);
-    if (get.rowCount > 0) return res.send({ error: 'User already exist' });
+    let fetchUser = [];
+
+    fetchUser = await connect.query(search, [email]);
+
+    if (fetchUser.rowCount > 0) return res.send({ error: 'User already exist' });
     try {
       const queries = sqlQueries.singUp;
       const { rows } = await connect.query(queries, data);
-
-      return res.status(200).send({
-        status: 200,
-        data: rows[0],
+      const token = helper.generateToken(rows[0].id);
+      return res.status(201).send({
+        status: 201,
+        data: rows[0].id,
+        token,
       });
+      // return res.status(200).send({
+      //   status: 200,
+      //   data: rows[0],
+      // });
     } catch (error) {
       return res.status(400).send({
         status: 400,
@@ -61,24 +75,38 @@ const user = {
       });
     }
   },
-
   // user sign in
   async signIn(req, res) {
     const { email, password } = req.body;
-    const data = [email, password];
-    try {
-      const search = sqlQueries.selectAll += ' AND password = $2';
-      let get = [];
-      get = await connect.query(search, data);
-      console.log(get);
-      if (get.rowCount > 0) {
-        return res.send({ data: 'User sign in successfully' });
-      }
-      return res.send({
-        error: 'user not found!!!',
 
+    if (!helper.isValidEmail(email)) {
+      return res.status(400).send({ message: 'Please enter a valid email address' });
+    }
+
+    try {
+      const search = sqlQueries.selectAll += 'AND password = $2';
+      let fetchUser = [];
+
+      fetchUser = await connect.query(search, [email, password]);
+      console.log(fetchUser);
+
+      if (fetchUser.rowCount > 0) {
+        const compare = helper.comparePassword(fetchUser.rows[0].password, password);
+
+        if (!compare) {
+          return res.status(400).send({ message: 'The credentials you provided is incorrect' });
+        }
+        const token = helper.generateToken(fetchUser.rows[0].id);
+        return res.send({
+          data: 'User sign in successfully',
+          token,
+        });
+      }
+      res.send({
+        error: 'User not found',
       });
     } catch (error) {
+      console.log(error);
       return res.send({
         error,
       });
